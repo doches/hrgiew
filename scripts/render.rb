3148423a2b0530866a9e6ee9845082c07884ce52
Trevor Fountain <doches@gmail.com>
2011-06-19 14:52:11 +0100

    If given a path, glob it; if given a prefix, glob that instead.
    Upshot is that render now accepts arguments of the form:
    
        ./render path/to/dir
    
    and
    
        ./render path/to/dir/prefix_for_files

--- scripts/render.rb
+++ scripts/render.rb
@@ -11,17 +11,22 @@ if ARGV.size != 1
     exit(0)
 end
 
-path = ARGV.shift
-Dir.foreach(path) do |file|
-	if file =~ /dot$/
-		full = File.join(path,file)
-		out = full.gsub(/dot$/,"png")
-		
-		if not File.exists?(out) or File.ctime(full) > File.ctime(out)
-			STDERR.puts "RENDER: #{full}"
-			`dot -Tpng #{full} -o #{out}`
-		else
-			STDERR.puts "SKIP:   #{full}"
-		end
+def render_file(full)
+  if full =~ /dot$/
+    out = full.gsub(/dot$/,"png")
+	
+  	if not File.exists?(out) or File.ctime(full) > File.ctime(out)
+  		STDERR.puts "RENDER: #{full}"
+  		`dot -Tpng #{full} -o #{out}`
+  	else
+  		STDERR.puts "SKIP:   #{full}"
+  	end
 	end
+end
+
+path = ARGV.shift
+if File.exists?(path)
+  Dir.foreach(path) { |file| render_file(File.join(path,file)) }
+else
+  Dir.glob("#{path}*").each { |file| render_file(file) }
 end
